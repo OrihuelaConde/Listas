48937811ec642815548f1470011e2e5d64d6ea3b
José Luis Orihuela Conde <orihuelaconde@gmail.com>
2021-09-20 14:03:02 -0300

    Agregar archivos de proyecto.

---
+++ TP1/Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1
+{
+    public class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Curso { get; set; }
+    }
+}
---
+++ TP1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidad;
+            List<Alumno> alumnos = new List<Alumno>();
+            Console.Write("Ingrese cantidad de alumnos: ");
+            cantidad = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 0; i < cantidad; i++)
+            {
+                Alumno alumno = new Alumno();                
+                Console.WriteLine($"Datos del alumno {i + 1}");
+                Console.Write("Nombre: ");
+                alumno.Nombre = Console.ReadLine();
+                Console.Write("Apellido: ");
+                alumno.Apellido = Console.ReadLine();
+                Console.Write("Curso: ");
+                alumno.Curso = Console.ReadLine();
+                Console.WriteLine();
+                alumnos.Add(alumno);
+            }
+            foreach(Alumno a in alumnos)
+            {
+                Console.WriteLine($"Datos del alumno {alumnos.IndexOf(a) + 1}");
+                Console.WriteLine($"Nombre: {a.Nombre}");
+                Console.WriteLine($"Apellido: {a.Apellido}");
+                Console.WriteLine($"Curso: {a.Curso}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
